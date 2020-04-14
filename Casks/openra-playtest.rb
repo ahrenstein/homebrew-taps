1fecec044b751c4394e4c39b57acba3371ce90f9
Matthew Ahrenstein <matt@ahrenstein.com>
2020-04-13 20:26:50 -0400

    OpenRA playtest

---
+++ Casks/openra-playtest.rb
@@ -0,0 +1,16 @@
+cask 'openra-playtest' do
+  version '20200329'
+  sha256 '28fe52f0fc0cfd18d7efeacacc93352245710902b41a1c29a5313e6494069cf1'
+
+  url "https://github.com/OpenRA/OpenRA/releases/download/playtest-#{version}/OpenRA-playtest-#{version}.dmg"
+  name 'openra-playtest'
+  homepage 'https://github.com/Inq8/CAmod'
+  appcast 'https://github.com/gatekeep/CAmod/releases.atom'
+  auto_updates false
+  conflicts_with cask: 'openra'
+
+  app 'OpenRA - Red Alert.app'
+  app 'OpenRA - Tiberian Dawn.app'
+  app 'OpenRA - Dune 2000.app'
+
+end

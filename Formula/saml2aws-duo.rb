4754eb6aa86ca0410683520aedc9152c503c72e0
Matthew Ahrenstein <matt@ahrenstein.com>
2021-03-29 18:57:54 -0400

    Push tag fix

--- Formula/saml2aws-duo.rb
+++ Formula/saml2aws-duo.rb
@@ -4,10 +4,10 @@ class Saml2awsDuo < Formula
   homepage 'https://github.com/versent/saml2aws'
   version '2.28.0-duo'
   if OS.mac?
-    url 'https://github.com/ahrenstein/homebrew-taps/releases/download/v1.0.1/saml2aws_2.28.0-duo_darwin_amd64.tar.gz'
+    url 'https://github.com/ahrenstein/homebrew-taps/releases/download/1.0.1/saml2aws_2.28.0-duo_darwin_amd64.tar.gz'
     sha256 '42c16edadc1e17ced46627f6944826250ec70bfce7675515f9fe58f611ac2bd8'
   elsif OS.linux?
-    url 'https://github.com/ahrenstein/homebrew-taps/releases/download/v1.0.1/saml2aws_2.28.0-duo_linux_amd64.tar.gz'
+    url 'https://github.com/ahrenstein/homebrew-taps/releases/download/1.0.1/saml2aws_2.28.0-duo_linux_amd64.tar.gz'
     sha256 'cb64313d819bb2546e454d9b4082363412c0ff8c4b5eae7317f7478418d713be'
   end
 
